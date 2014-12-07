1dc201d23bd4905dcf9c39a232506186468f11cb
Benjamin Daschel <benjamin.daschel@limelyte.com>
2014-12-07 12:23:40 -0800

    a really basic page that shows all the orders

--- index.php
+++ index.php
@@ -9,10 +9,30 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/templates',
 ));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'user' => 'homestead',
+        'password' => 'secret',
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'SalesOrders'
+     ),
+));
+
+$app->get('/', function () use ($app){
+    $sql = 'select * from Invoice';
+    /** @var \Doctrine\DBAL\Connection $conn */
+    $conn = $app['db'];
+    $orders = $conn->fetchAll($sql);
+    return $app['twig']->render('orders.twig', array('orders'=>$orders));
+});
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return $app['twig']->render('index.twig', array('name'=>$name));
