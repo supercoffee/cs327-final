33e9ee90a65c975e695dbb699ef20d776069ad74
Benjamin Daschel <benjamin.daschel@limelyte.com>
2014-12-07 14:20:31 -0800

    some more features and stuff

--- index.php
+++ index.php
@@ -27,11 +27,51 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 ));
 
 $app->get('/', function () use ($app){
-    $sql = 'select * from Invoice';
+    $sql = 'select * from Invoice join Customer on Invoice.CustNum = Customer.CustNum';
     /** @var \Doctrine\DBAL\Connection $conn */
     $conn = $app['db'];
     $orders = $conn->fetchAll($sql);
-    return $app['twig']->render('orders.twig', array('orders'=>$orders));
+
+    $sql = 'select * from InvoiceLineItem join Inventory on Inventory.SKU = InvoiceLineItem.SKU';
+    $items = $conn->fetchAll($sql);
+    $summary = array('sales'=>0, 'weight'=>0, 'items'=>0);
+    foreach ($items as $item){
+        $summary['sales'] += $item['UnitPrice'] * $item['OrdQty'];
+        $summary['weight'] += $item['UnitWeight'] * $item['OrdQty'];
+        $summary['items'] += 1;
+    }
+    return $app['twig']->render('index.twig', array('orders'=>$orders, 'summary'=> $summary));
+});
+
+$app->get('/order/{id}', function ($id) use ($app){
+    $sql = 'select * from Invoice join Customer on Invoice.CustNum = Customer.CustNum where OrderNum = ?';
+    /** @var \Doctrine\DBAL\Connection $conn */
+    $conn = $app['db'];
+    $order = $conn->fetchAssoc($sql, array($id));
+
+    $sql = 'select * from InvoiceLineItem join Inventory on Inventory.SKU = InvoiceLineItem.SKU where OrderNum = ?';
+    $items = $conn->fetchAll($sql, array($id));
+    $orderTotal = 0;
+    foreach($items as $item){
+        $orderTotal += $item['OrdQty'] * $item['UnitPrice'];
+    }
+    return $app['twig']->render('order_details.twig',
+        array('order'=>$order, 'items'=>$items, 'total'=> $orderTotal));
+});
+
+$app->get('/customer/{id}', function ($id) use ($app){
+
+    $sql = 'select * from Customer where CustNum = ?';
+    /** @var \Doctrine\DBAL\Connection $conn */
+    $conn = $app['db'];
+    $customer = $conn->fetchAssoc($sql, array($id));
+
+    $sql = 'select * from InvoiceLineItem
+        join Inventory on Inventory.SKU = InvoiceLineItem.SKU
+        where InvoiceLineItem.OrderNum in (select OrderNum from Invoice where Invoice.CustNum = ?)';
+    $items = $conn->fetchAll($sql, array($id));
+
+    return $app['twig']->render('customer.twig', array('customer'=>$customer, 'items'=>$items));
 });
 
 $app->run();
